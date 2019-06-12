495a0ddabda96d4806777d695e305c1942b0d0c6
Ralf <none@none>
2019-06-12 11:22:34 +0200

    Versionsnummer MODVERSION aktualisieren

--- VSDrv.h
+++ VSDrv.h
@@ -22,7 +22,7 @@
 
 //> defines about the Module
 /******************************************************************************************/
-#define MODVERSION "1.0.0.1"
+#define MODVERSION "1.0.0.2"
 #define MODDATECODE __DATE__ " - " __TIME__
 #define MODLICENSE "GPL";
 #define MODDESCRIPTION "Kernel module for the VisionSensor VSPV VPFE(e) devices";
